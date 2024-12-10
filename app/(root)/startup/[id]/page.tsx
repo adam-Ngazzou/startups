b418aa4fd8c3735115d56ba74ae3e92410340f78
AdamNgazzou <adem.ng123@gmail.com>
2024-12-11 00:38:59 +0100

    heheh

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,28 +1,31 @@
+import { Suspense } from "react";
+import { client } from "@/sanity/lib/client";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+import { formatDate } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
-import { Suspense } from "react";
+
 import markdownit from "markdown-it";
-import { notFound } from "next/navigation";
-
-import { formatDate } from "@/lib/utils";
-import {
-  STARTUP_BY_ID_QUERY,
-  PLAYLIST_BY_SLUG_QUERY,
-} from "@/sanity/lib/queries";
-import { client } from "@/sanity/lib/client";
-
-import View from "@/components/View";
 import { Skeleton } from "@/components/ui/skeleton";
+import View from "@/components/View";
 import StartupCard, { StartupCardType } from "@/components/StartupCard";
 
 const md = markdownit();
 
-async function Page({ params }: { params: { id: string } }) {
-  const { id } =  params;
+export const experimental_ppr = true;
 
-  const [post, editorPosts] = await Promise.all([
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+
+  const [post, { select: editorPosts }] = await Promise.all([ // parallel loading is significantly faster than sequential loading // 
     client.fetch(STARTUP_BY_ID_QUERY, { id }),
-    client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks" }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks",
+    }),
   ]);
 
   if (!post) return notFound();
@@ -32,7 +35,8 @@ async function Page({ params }: { params: { id: string } }) {
   return (
     <>
       <section className="pink_container !min-h-[230px]">
-        <p className="tag">{formatDate(post._createdAt)}</p>
+        <p className="tag">{formatDate(post?._createdAt)}</p>
+
         <h1 className="heading">{post.title}</h1>
         <p className="sub-heading !max-w-5xl">{post.description}</p>
       </section>
@@ -40,7 +44,7 @@ async function Page({ params }: { params: { id: string } }) {
       <section className="section_container">
         <img
           src={post.image}
-          alt="image"
+          alt="thumbnail"
           className="w-full h-auto rounded-xl"
         />
 
@@ -52,11 +56,12 @@ async function Page({ params }: { params: { id: string } }) {
             >
               <Image
                 src={post.author.image}
-                alt="image"
+                alt="avatar"
                 width={64}
                 height={64}
                 className="rounded-full drop-shadow-lg"
               />
+
               <div>
                 <p className="text-20-medium">{post.author.name}</p>
                 <p className="text-16-medium !text-black-300">
@@ -84,20 +89,21 @@ async function Page({ params }: { params: { id: string } }) {
         {editorPosts?.length > 0 && (
           <div className="max-w-4xl mx-auto">
             <p className="text-30-semibold">Editor Picks</p>
+
             <ul className="mt-7 card_grid-sm">
-              {editorPosts.map((post: StartupCardType, index: number) => (
-                <StartupCard key={index} post={post} />
+              {editorPosts.map((post: StartupCardType, i: number) => (
+                <StartupCard key={i} post={post} />
               ))}
             </ul>
           </div>
         )}
-      </section>
 
-      <Suspense fallback={<Skeleton className="view_skeleton" />}>
-        <View id={id} />
-      </Suspense>
+        <Suspense fallback={<Skeleton className="view_skeleton" />}>
+          <View id={id} />
+        </Suspense>
+      </section>
     </>
   );
-}
+};
 
 export default Page;
